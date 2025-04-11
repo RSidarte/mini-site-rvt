4ba6596f8b89f9811b125fa1a6af430fdb6808e4
Sidarte Rangadamalou <Sidarte@MacBook-Air-de-Elyse.local>
2025-04-11 12:39:34 +0200

    🚀 mini site React Dynamic Search added

--- src/pages/Projets.jsx
+++ src/pages/Projets.jsx
@@ -1,5 +1,6 @@
 import PageWrapper from '../components/PageWrapper';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const projets = [
     {
@@ -15,15 +16,34 @@ const projets = [
       image: 'https://picsum.photos/id/1016/400/300',
     },
   ];
+
+
   
 
 function Projets() {
+    const [search, setSeach] = useState('');
+
+    const filteredProjects = projets.filter((projet) =>
+        projet.title.toLowerCase().includes(search.toLowerCase()) ||
+        projet.description.toLowerCase().includes(search.toLowerCase())
+      );
+      
   return (
     <PageWrapper>
       <section className="min-h-screen bg-gray-100 py-12 px-4">
         <h2 className="text-3xl font-bold text-center mb-10">Mes Projets</h2>
+        <div className="max-w-md mx-auto mb-10">
+            <input
+                type="text"
+                placeholder="Rechercher un projet..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="w-full p-2 border rounded shadow-sm"
+            />
+        </div>
+
         <div className="flex flex-wrap gap-8 justify-center">
-          {projets.map((projet) => (
+          {filteredProjects.map((projet) => (
             <div
               key={projet.id}
               className="bg-white shadow-md rounded-lg w-80 overflow-hidden hover:shadow-lg transition-shadow duration-300"
