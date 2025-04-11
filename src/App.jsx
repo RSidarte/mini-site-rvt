2bb8b99ce390a31dc4f23f6467a50004fd8c50e6
Sidarte Rangadamalou <Sidarte@MacBook-Air-de-Elyse.local>
2025-04-11 11:59:44 +0200

    🚀 mini site React Animation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { AnimatePresence } from 'framer-motion';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Equipe from './pages/Equipe';
 import ContactPage from './pages/ContactPage';
 
-function App() {
+function AnimatedRoutes() {
   const location = useLocation();
 
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/equipe" element={<Equipe />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function App() {
   return (
     <Router>
       <Navbar />
       <main className="pt-20">
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/equipe" element={<Equipe />} />
-            <Route path="/contact" element={<ContactPage />} />
-          </Routes>
-        </AnimatePresence>
+        <AnimatedRoutes />
       </main>
       <Footer />
     </Router>
